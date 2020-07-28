136620976954f83e91da045f19a4e6b4e45ed591
earlgreybutter <duckduck31216@gmail.com>
2020-07-28 10:42:32 +0900

    backendfinish

--- src/main/java/com/example/cococat/services/LoginFilter.java
+++ src/main/java/com/example/cococat/services/LoginFilter.java
@@ -1,12 +1,18 @@
 package com.example.cococat.services;
 
 import java.io.IOException;
+import java.util.Collections;
 
+import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.example.cococat.domain.UserCredentials;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
@@ -14,7 +20,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 public class LoginFilter extends AbstractAuthenticationProcessingFilter {
 
-    protected LoginFilter(String url, AuthenticationManager authManager) {
+    public LoginFilter(String url, AuthenticationManager authManager) {
         super(new AntPathRequestMatcher(url));
         setAuthenticationManager(authManager);
     }
@@ -22,8 +28,14 @@ public class LoginFilter extends AbstractAuthenticationProcessingFilter {
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
             throws AuthenticationException, IOException, ServletException {
-        // TODO Auto-generated method stub
-        return null;
+        
+        UserCredentials userCredentials = new ObjectMapper().readValue(request.getInputStream(), UserCredentials.class);
+        return getAuthenticationManager().authenticate(new UsernamePasswordAuthenticationToken(userCredentials.getUserName(), userCredentials.getPassword(), Collections.emptyList()) );
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication auth ){
+        AuthenticationService.addJWTToken(response, auth.getName());
     }
     
     
